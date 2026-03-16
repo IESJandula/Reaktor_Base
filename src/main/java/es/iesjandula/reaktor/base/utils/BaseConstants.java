6d4559943c18b90cf55aeca58d9736fe721fa46d
Paco Benitez Chico <paco.benitez.chico@gmail.com>
2026-03-16 11:10:17 +0100

    Nuevo rol Friends & Family para que solo unos pocos profes puedan ir probando la apertura de puertas

--- src/main/java/es/iesjandula/reaktor/base/utils/BaseConstants.java
+++ src/main/java/es/iesjandula/reaktor/base/utils/BaseConstants.java
@@ -79,6 +79,9 @@ public class BaseConstants
 	
 	/** Role - Dirección */
 	public static final String ROLE_DIRECCION 	  	          = "DIRECCION" ;
+
+	/** Role - Friends and Family */
+	public static final String ROLE_FRIENDS_AND_FAMILY 	  	  = "FRIENDS_AND_FAMILY" ;
 	
 	/** Role - Cliente impresora */
 	public static final String ROLE_CLIENTE_IMPRESORA         = "CLIENTE_IMPRESORA" ;
@@ -105,7 +108,7 @@ public class BaseConstants
 	public static final String ROLE_APLICACION_SCHOOL_BASE    = "APLICACION_SCHOOL_BASE" ;
 	
 	/** Lista de roles permitidos para usuarios web */
-	public static final List<String> ROLES_USUARIOS_LIST      = Arrays.asList(new String[] { ROLE_ADMINISTRADOR, ROLE_PROFESOR, ROLE_DIRECCION }) ;
+	public static final List<String> ROLES_USUARIOS_LIST      = Arrays.asList(new String[] { ROLE_ADMINISTRADOR, ROLE_PROFESOR, ROLE_DIRECCION, ROLE_FRIENDS_AND_FAMILY }) ;
 
 	/** Lista de roles permitidos para aplicaciones */
 	public static final List<String> ROLES_APPS_LIST          = Arrays.asList(new String[] { ROLE_CLIENTE_IMPRESORA, ROLE_CLIENTE_PROYECTOR, ROLE_CLIENTE_PUERTA, 
